e8f46c6b52028985dbd2f883aa6c46c33ab6cf20
stefanvh87 <stefanvh87@gmail.com>
2022-08-28 19:47:43 +0200

    cleaning

--- lib/main.dart
+++ lib/main.dart
@@ -10,39 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget titleSection = Container(
-      padding: const EdgeInsets.all(32),
-      child: Row(
-        children: [
-          Expanded(
-            /*1*/
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                /*2*/
-                Container(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: const Text(
-                    'Dispenser 1',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                const ChoiceChip(label: Text('5ml'), selected: true)
-              ],
-            ),
-          ),
-          /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          const Text('41'),
-        ],
-      ),
-    );
-
     return MaterialApp(
       title: 'command interface demo',
       home: Scaffold(
@@ -52,17 +19,9 @@ class MyApp extends StatelessWidget {
         body: ListView(
           children: [
             ConnectWidget(),
-            titleSection,
           ],
         ),
       ),
     );
   }
 }
-
-// Widget buttonConnect = Row(
-//   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//   children: const [
-//     ConnectWidget(),
-//   ],
-// );
