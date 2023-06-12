0e60af519a7e6607adb56c6dae017fd53afcda83
Anvh87 <stefanvh87@gmail.com>
2023-06-12 13:06:25 +0200

    changed to serial flutter package

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,59 @@
 import 'package:flutter/material.dart';
-import 'package:command_interface/widgets/serial_connect';
+import 'widgets/serial_cmd.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({Key? key}) : super(key: key);
+  SerialCMD serialCMD = SerialCMD();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'command interface demo',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('command interface demo'),
+          title: const Text('Flutter Serial'),
         ),
-        body: ListView(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            ConnectWidget(),
+            Expanded(
+              child: ListView(
+                padding: const EdgeInsets.all(8),
+                children: serialCMD.received.map((e) => Text(e)).toList(),
+              ),
+            ),
+            ElevatedButton(
+              child: const Text('Open Port'),
+              onPressed: () {
+                serialCMD.openPort();
+              },
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            ),
+            TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter commands',
+                ),
+                onChanged: (text) {
+                  serialCMD.msgBuffer = text;
+                },
+                onEditingComplete: () {
+                  serialCMD.writeToPort();
+                }),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            ),
+            ElevatedButton(
+              child: const Text('Send'),
+              onPressed: () {
+                serialCMD.writeToPort();
+              },
+            ),
           ],
         ),
       ),
---
+++ lib/widgets/serial_cmd.dart
@@ -0,0 +1,48 @@
+import 'dart:typed_data';
+import 'dart:html';
+import 'dart:async';
+
+import 'package:serial/serial.dart';
+
+class SerialCMD {
+  final received = <String>[];
+  SerialPort? _port;
+  String msgBuffer = '';
+
+  Future<void> openPort() async {
+    final port = await window.navigator.serial.requestPort();
+    await port.open(baudRate: 9600);
+
+    _port = port;
+    readFromPort();
+  }
+
+  Future<void> writeToPort() async {
+    if (_port == null) {
+      return;
+    }
+
+    final writer = _port!.writable.writer;
+
+    await writer.ready;
+    await writer.write(Uint8List.fromList('$msgBuffer\r'.codeUnits));
+
+    await writer.ready;
+    await writer.close();
+  }
+
+  Future<void> readFromPort() async {
+    if (_port == null) {
+      return;
+    }
+
+    final reader = _port!.readable.reader;
+
+    while (true) {
+      final result = await reader.read();
+      final text = String.fromCharCodes(result.value);
+
+      received.add(text);
+    }
+  }
+}
